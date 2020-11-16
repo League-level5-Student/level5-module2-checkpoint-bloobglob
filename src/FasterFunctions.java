f24f14c9d3d9f81c84a4daedb3b083fedb78b43c
bloobglob <lucas.joel.han@gmail.com>
2020-11-16 15:39:01 -0800

    fin

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -4,10 +4,25 @@ public class FasterFunctions {
 		float totalTime = 0;
 		long startTime = System.currentTimeMillis();
 		//Your Code Here
-		
-		
-		
-		
+		Thread slowSortLargeArray = new Thread(()->{
+			SlowFunctions.slowSortLargeArray();
+		});
+		Thread ackermann = new Thread(()->{
+			SlowFunctions.ackermann(3, 14);
+		});
+		Thread millionsOfSqrts = new Thread(()->{
+			SlowFunctions.millionsOfSqrts();
+		});
+		slowSortLargeArray.start();
+		ackermann.start();
+		millionsOfSqrts.start();
+		try {
+			slowSortLargeArray.join();
+			ackermann.join();
+			millionsOfSqrts.join();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
 		totalTime += endTime;
 		System.out.println("Total Time: " + totalTime);
